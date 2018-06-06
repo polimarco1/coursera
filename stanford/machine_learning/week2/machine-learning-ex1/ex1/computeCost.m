677b7509b7490ef20a9ef7a813e7ebafeac3f8f1
polimarco1 <polimarco1@gmail.com>
2018-06-06 13:58:57 -0700

    computeCost.m before submit

--- stanford/machine_learning/week2/machine-learning-ex1/ex1/computeCost.m
+++ stanford/machine_learning/week2/machine-learning-ex1/ex1/computeCost.m
@@ -3,15 +3,34 @@ function J = computeCost(X, y, theta)
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
 
+% Loading and setting up the data
+data = load('ex1data1.txt');
+y = data(:, 2);
+
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+
+
+% Only to test the script. It should be removed to submit
+X = [ones(m, 1), data(:, 1)];   % Add a column of ones to x
+theta = zeros(2, 1);            % Initialize fitting parameters
+iterations = 1500;
+alpha = 0.01;
+
+sigma = 0;
+for i = 1:m,
+    sigma = sigma + (theta(1, 1)*X(i, 1) + theta(2, 1)*X(i, 2) - y(i))^2;
+end
+
+J = (1/(2*m))*sigma;
+
 
 
 
