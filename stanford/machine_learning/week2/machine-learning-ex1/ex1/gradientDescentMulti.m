f237a075e56117fa951be29b518be0940aec5e35
polimarco1 <polimarco1@gmail.com>
2018-06-06 19:08:30 -0700

    working on gradient discent m

--- stanford/machine_learning/week2/machine-learning-ex1/ex1/gradientDescentMulti.m
+++ stanford/machine_learning/week2/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -11,14 +11,15 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
 
-
+    s = zeros(size(theta, 1), 1);
+    for i = 1:m
 
 
 
@@ -29,7 +30,7 @@ for iter = 1:num_iters
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
