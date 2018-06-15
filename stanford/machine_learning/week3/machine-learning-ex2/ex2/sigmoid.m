0430c7b366a80f933017901ac1e06b1e0ffa9eab
polimarco1 <polimarco1@gmail.com>
2018-06-15 16:39:09 -0700

    sigmoid implemented

--- stanford/machine_learning/week3/machine-learning-ex2/ex2/sigmoid.m
+++ stanford/machine_learning/week3/machine-learning-ex2/ex2/sigmoid.m
@@ -2,7 +2,7 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid function
 %   g = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
@@ -10,7 +10,15 @@ g = zeros(size(z));
 %               vector or scalar).
 
 
+for i = 1:size(z, 1),
 
+    for j = 1:size(z, 2),
+
+        g(i, j) = 1/(1+e^(-z(i, j)));
+
+    end
+
+end
 
 
 % =============================================================
