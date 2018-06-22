3b2025cdbed5f2263083521bf7c437e709a66300
polimarco1 <polimarco1@gmail.com>
2018-06-22 15:42:05 -0700

    lrCostFunction.m done

--- stanford/machine_learning/week4/machine-learning-ex3/ex3/lrCostFunction.m
+++ stanford/machine_learning/week4/machine-learning-ex3/ex3/lrCostFunction.m
@@ -1,14 +1,14 @@
 function [J, grad] = lrCostFunction(theta, X, y, lambda)
-%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with
 %regularization
 %   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -19,31 +19,43 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 %
 % Hint: The computation of the cost function and gradients can be
-%       efficiently vectorized. For example, consider the computation
+%      efficiently vectorized. For example, consider the computation
 %
 %           sigmoid(X * theta)
 %
 %       Each row of the resulting matrix will contain the value of the
 %       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
+%       the cost function and gradient computations.
 %
-% Hint: When computing the gradient of the regularized cost function, 
+% Hint: When computing the gradient of the regularized cost function,
 %       there're many possible vectorized solutions, but one solution
 %       looks like:
 %           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           temp = theta;
+%           temp(1) = 0;   % because we don't add anything for j = 0
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
 
+% One line implementation
+% J = (-y'*log(sigmoid(X*theta))-(1-y')*log(1-sigmoid(X*theta)))/m ...
+% + (lambda/(2*m))*sum(theta(2:end).*theta(2:end));
+
+% Breaking it into functions
+
+Z = (X*theta);
+H = sigmoid(Z);
+j = theta(2:end);
+SJ = ( lambda/(2*m) )*sum( j.*j );
+
+J = (1/m)*( -y'*log(H) - ( 1-y' )*log( 1-H ) ) + SJ;
 
 
+j = theta;
+j(1) = 0;
+SG = (lambda/m)*j;
 
-
-
-
-
+grad = ( ( (H-y)'*X )'/m ) + SG ;
 
 % =============================================================
 
